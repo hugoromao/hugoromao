7d0adc688dfd56c8f3b2d1fe0bfb95520022a3b4
Hugo Lima Romão <hugo8romao@gmail.com>
2023-12-11 08:09:40 -0400

    style: eslint, prettier setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode:true,
+    swcMinify: true,
+    images: {
+        formats: ["image/avif", "image/webp"],
+        dangerouslyAllowSVG: true,
+        remotePatterns: []
+      },
+}
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: colors.neutral,
+      },
+      fontFamily: {
+        // to change, update font in _document.js
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--font-lora)", ...defaultTheme.fontFamily.serif],
+        stock: [defaultTheme.fontFamily.sans],
+      },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+        "2/3": "2 / 3",
+        "9/16": "9 / 16",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
+export default config;
