cd17f4a4355e8aacd606977aa2cd0e8cf82d58d3
Hugo Lima Romão <hugo8romao@gmail.com>
2023-12-13 13:02:19 -0400

    fix: hotjar

--- src/app/[locale]/(website)/layout.tsx
+++ src/app/[locale]/(website)/layout.tsx
@@ -75,23 +75,16 @@ export default function Layout({
 
       <Footer />
 
-      <Script
-        id="HotJarAnalytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `<!-- Hotjar Tracking Code for hugoromao -->
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3793342,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>`
-        }}
-      />
+      <Script id="HotJarAnalytics" strategy="afterInteractive">
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3793342,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
     </>
   );
 }
