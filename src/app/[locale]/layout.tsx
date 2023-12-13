de7cc5bf34901aa5655cfe1644fe1205ccff76a4
Hugo Lima Romão <hugo8romao@gmail.com>
2023-12-13 12:13:54 -0400

    ci: google verification

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { Inter, Lora } from 'next/font/google';
 import { Providers } from './providers';
 
 import { cx } from '@/utils/all';
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -28,6 +29,12 @@ export default function RootLayout({
       lang={locale}
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="Qufe6ZpDpJZ659WymsEhkTVRMrdkF8rG887Sdm9iDiM"
+        />
+      </Head>
       <body className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
         <Providers>{children}</Providers>
       </body>
