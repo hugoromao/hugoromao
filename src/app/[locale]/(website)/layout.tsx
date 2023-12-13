ea5a700d3e62925fbc0b1725d69f8c211630c4d6
Hugo Lima Romão <hugo8romao@gmail.com>
2023-12-13 13:04:57 -0400

    fix: setting new hotjarid

--- src/app/[locale]/(website)/layout.tsx
+++ src/app/[locale]/(website)/layout.tsx
@@ -67,6 +67,8 @@ export default function Layout({
     }
   ];
 
+  const hotjarID = process.env.NEXT_PUBLIC_HOTJAR_ID;
+
   return (
     <>
       <Navbar leftmenu={leftmenu} rightmenu={rightmenu} />
@@ -78,7 +80,7 @@ export default function Layout({
       <Script id="HotJarAnalytics" strategy="afterInteractive">
         {`(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3793342,hjsv:6};
+        h._hjSettings={hjid:${hotjarID},hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
